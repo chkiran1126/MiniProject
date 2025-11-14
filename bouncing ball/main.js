53ece91e3429d6a22323c054b30b042ce48d4004
chkiran1126 <chkiran1126@gmail.com>
2025-11-14 23:05:07 +0530

    Add start and stop functionality for ball animation

--- bouncing ball/main.js
+++ bouncing ball/main.js
@@ -1,1 +1,12 @@
+const ball = document.querySelector(".ball");
+const startBtn = document.getElementById("start-btn");
+const stopBtn = document.getElementById("stop-btn");
+
+startBtn.addEventListener("click", () => {
+  ball.style.animationPlayState = "running";
+});
+
+stopBtn.addEventListener("click", () => {
+  ball.style.animationPlayState = "paused";
+});
 
